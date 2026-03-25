04f19122b76c145bc8103258f460c573c22eaee8
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-25 11:30:15 +0530

    Add index.ts to implement a greeting function and demonstrate TypeScript usage

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function greet(name: string): string {
+    return `Hello ${name}, Welcome to the world of TypeScript!`;
+}
+
+const username: string =  "John Doe";
+
+console.log(greet(username));
+console.log(greet("shreyesh"));
