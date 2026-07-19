c9c05bd0761d8fb26d2411e6847878580fd538a6
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-20 01:34:29 +0530

    Refactor equality check in mini_challenge.ts to use strict equality (===) for better type safety

--- Module_2/src/mini_challenge.ts
+++ Module_2/src/mini_challenge.ts
@@ -6,7 +6,31 @@ age = "Shreyesh"
 
 let name:unknown = "TypeScript";
 
-if(typeof name == "string" ){
+if(typeof name === "string" ){ 
+//  //(//🔥 Tiny Improvement
+
+// Instead of
+
+// if(typeof name == "string")
+
+// prefer
+
+// if (typeof name === "string")
+
+// Why?
+
+// Because in JavaScript/TypeScript:
+
+// == → loose equality
+// === → strict equality
+
+// Professional codebases almost always use ===.
+
+// So write:
+
+// if (typeof name === "string") {
+//     console.log(name.toUpperCase());
+// })
     
     console.log(name.toUpperCase());
     
