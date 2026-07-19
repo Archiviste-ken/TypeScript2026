5983ab157e5ef9e4ea78ad20241eeddcdec41988
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-20 01:01:45 +0530

    Add boss_challenge.ts and mini_challenge.ts for variable declaration exercises; create typeSafety.txt to illustrate TypeScript's type safety features

---
+++ Module_1/src/boss_challenge.ts
@@ -0,0 +1,21 @@
+// 👑 Boss Challenge
+
+// Create these variables using explicit types:
+
+// Company Name
+
+// Employee Count
+
+// Is Hiring
+
+// CEO Name
+
+// Then print them.
+
+let companyName: string = "Shreyesh Tech ltd"
+let employeeCount: number = 45
+let isHiring: boolean = true
+let ceoName: string = "Shreyesh Pachpor"
+
+
+console.log(companyName,employeeCount,isHiring,ceoName);
---
+++ Module_1/src/mini_challenge.ts
@@ -0,0 +1,19 @@
+// ⚔️ Mini Challenge (5 Minutes)
+
+// Create these variables:
+
+// Your name
+// Your age
+// Whether you are learning TypeScript
+// Your favorite programming language
+
+// Use type inference only (don't write : string, : number, etc.).
+
+// Then print them.
+
+let name = "Shreyesh";
+let age = 21;
+let isLearningTS = true;
+let favProgrammingLanguage = "python";
+
+console.log(name, age, isLearningTS, favProgrammingLanguage);
