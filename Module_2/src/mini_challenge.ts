48b7e0c3180c7c59865dbea37b56c9e8d95dc322
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-20 01:28:39 +0530

    Add initial project setup with TypeScript configuration, package management, and example challenges

---
+++ Module_2/src/mini_challenge.ts
@@ -0,0 +1,15 @@
+let age:any = 21;
+
+age = "Shreyesh"
+
+// console.log(age) => type error
+
+let name:unknown = "TypeScript";
+
+if(typeof name == "string" ){
+    
+    console.log(name.toUpperCase());
+    
+}
+
+
