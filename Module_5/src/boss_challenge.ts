093a8d20b20c6b5fdcdfbe59c161f1da27d49fe3
Shreyesh <shreyeshpachpor06@gmail.com>
2026-08-17 23:41:25 +0530

    Add boss and mini challenge files with Employee and Car types; update tsconfig for directory settings

---
+++ Module_5/src/boss_challenge.ts
@@ -0,0 +1,52 @@
+// 👑 Boss Challenge (10 Minutes)
+// Create a type:
+// Employee
+// Properties:
+// readonly id
+// name
+// department
+// salary
+// phone (optional)
+// Now create:
+// employees
+// An array of three Employee objects.
+// Print the array.
+
+type Employee = {
+    readonly id: string,
+    name: string,
+    department: string,
+    salary: number,
+    phone ?: number,
+}
+
+const employees: Employee = {
+    id: "23513656",
+    name: "John Smith",
+    department: "Marketting department",
+    salary: 1000000,
+}
+
+console.log(employees);
+
+const Emps: Employee[] = [
+    {
+        //
+    },
+    {
+        //
+    },
+    {
+        //
+    },
+]
+
+console.log(Emps);
+
+
+
+
+
+
+
+
---
+++ Module_5/src/mini_challenge.ts
@@ -0,0 +1,29 @@
+// ⚔️ Mini Challenge (5 Minutes)
+// Create a type:
+// Car
+// Properties:
+// brand
+// model
+// year
+// Create one object and print it.
+// Create an array:
+// favoriteNumbers
+// Store five numbers and print them.
+
+type car = {
+  brand: string;
+  model: string;
+  year: number;
+};
+
+const alto: car = {
+  brand: "maruti suzuki",
+  model: "alto 800",
+  year: 2005,
+};
+
+console.log(alto);
+
+const numbers: number[] = [1, 2, 3];
+
+console.log(numbers);
