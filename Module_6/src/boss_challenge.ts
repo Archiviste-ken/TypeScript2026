f5f9a4738eaef719245d2d187799fa6f9ba9f317
Shreyesh <shreyeshpachpor06@gmail.com>
2026-08-18 00:18:44 +0530

    dayum 😫

---
+++ Module_6/src/boss_challenge.ts
@@ -0,0 +1,19 @@
+// 👑 Boss Challenge (10 Minutes)
+// Create these interfaces:
+// Person
+// name
+// age
+// Student
+// extends Person
+// Add:
+// course
+// semester
+// Teacher
+// extends Person
+// Add:
+// subject
+// salary
+// Now create:
+// one Student object
+// one Teacher object
+// Print both.
